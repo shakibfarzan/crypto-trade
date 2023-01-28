2c433c8c1a922e127a658da461dc1f3025649553
shakibfn <shakib@greenhabitat.at>
2023-01-28 20:19:00 +0330

    add filters

--- app/views.py
+++ app/views.py
@@ -10,6 +10,15 @@ class WatchListView(MultipleObjectTemplateResponseMixin, View):
 
     def get(self, request):
         search = request.GET.get('search', False)
-        watchlist = get_watchlist(search)
-        ctx = { 'watchlist': watchlist, 'search': search }
+        vol_change_min = request.GET.get('vol_change_min', default=None)
+        dom_min = request.GET.get('dom_min', default=None)
+        vol_per_mcap_min = request.GET.get('vol_per_mcap_min', default=None)
+        watchlist = get_watchlist(search, vol_change_min, dom_min, vol_per_mcap_min)
+        ctx = { 
+            'watchlist': watchlist, 
+            'search': search,
+            'vol_change_min': vol_change_min,
+            'dom_min': dom_min,
+            'vol_per_mcap_min': vol_per_mcap_min
+            }
         return render(request, self.template_name, ctx)
